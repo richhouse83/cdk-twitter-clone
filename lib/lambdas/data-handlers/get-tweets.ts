d253d18266c0d3777002ed6ab40b27ddc52c5e98
Rich House <rich@richhouse.co.uk>
2023-01-05 10:47:24 +0000

    adds get tweets by username

--- lib/lambdas/data-handlers/get-tweets.ts
+++ lib/lambdas/data-handlers/get-tweets.ts
@@ -2,7 +2,43 @@ import { APIGatewayProxyResult } from "aws-lambda";
 import { DynamoDB } from "aws-sdk";
 
 export const getTweets = async (path: string, tableName: string, docClient: DynamoDB.DocumentClient): Promise<APIGatewayProxyResult> => {
-  
+
+  const userId = path.replace('/tweets', '').replace(/\//g, '')
+
+  if (userId) {
+    const queryParams: DynamoDB.DocumentClient.QueryInput = {
+      TableName: tableName,
+      KeyConditionExpression: 'UserId = :uid',
+      ExpressionAttributeValues: {
+        ':uid': userId
+      }
+    }
+
+    try {
+      const results: DynamoDB.DocumentClient.ScanOutput = await docClient.query(queryParams).promise();
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET"
+        },
+        body: JSON.stringify(results)
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET"
+        },
+        body: JSON.stringify(error)
+      }
+    }
+  }
+
+
   const scanParams: DynamoDB.DocumentClient.ScanInput = {
     TableName: tableName
   }
@@ -12,20 +48,20 @@ export const getTweets = async (path: string, tableName: string, docClient: Dyna
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET"
-    },
+      },
       body: JSON.stringify(results)
     }
   } catch (error) {
     return {
       statusCode: 500,
       headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET"
-    },
+      },
       body: JSON.stringify(error)
     }
   }
--- test/lambdas/data-handlers/get-tweets.test.ts
+++ test/lambdas/data-handlers/get-tweets.test.ts
@@ -1,6 +1,6 @@
 import * as AWSMock from 'aws-sdk-mock';
 import * as AWS from 'aws-sdk';
-import { GetItemInput, ScanInput, ScanOutput } from 'aws-sdk/clients/dynamodb';
+import { GetItemInput, QueryInput, ScanInput, ScanOutput } from 'aws-sdk/clients/dynamodb';
 import {getTweets} from '../../../lib/lambdas/data-handlers/get-tweets';
 
 describe('get-tweets', () => {
@@ -29,5 +29,54 @@ describe('get-tweets', () => {
 
       expect(result.statusCode).toBe(200);
       expect(JSON.parse(result.body)).toHaveProperty('Items');
+    
+    AWSMock.restore();
   })
+
+  test('If Document Client errors during scan, returns 500 status Code', async () => {
+    const client = new AWS.DynamoDB.DocumentClient();
+
+    const result = await getTweets('/tweets', 'tableName', client);
+
+    expect(result.statusCode).toBe(500);
+  });
+
+  test('Performs a query when userId is supplied', async () => {
+
+    const returnData = {
+      Items: [
+        { UserId: 'testId', TweetId: 1, Tweet: "tweet"}
+      ]
+    }
+
+    let queryParams: QueryInput = {
+      TableName: ''
+    };
+    
+    AWSMock.mock('DynamoDB.DocumentClient', 'query', (params: QueryInput, callback: Function) => {
+      queryParams = params;
+      callback(null, returnData);
+    });
+
+    const client = new AWS.DynamoDB.DocumentClient();
+
+    const testName = 'Test.Name'
+
+    const result = await getTweets('/tweets/' + testName, 'tableName', client)
+
+    expect(queryParams?.ExpressionAttributeValues?.[':uid']).toBe(testName)
+
+    expect(result.statusCode).toBe(200);
+    expect(JSON.parse(result.body)).toHaveProperty('Items');
+
+    AWSMock.restore();
+  });
+
+  test('If Document Client errors during query, returns 500 status Code', async () => {
+    const client = new AWS.DynamoDB.DocumentClient();
+
+    const result = await getTweets('/tweets/testname', 'tableName', client);
+
+    expect(result.statusCode).toBe(500);
+  });
 })
